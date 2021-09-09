b372838fc5af6075c4133835d677f52bee87f01e
Sajan <sajanmhatre29@gmail.com>
2021-09-09 13:59:24 +0530

    Files Saved

--- src/DeckOfCard.java
+++ src/DeckOfCard.java
@@ -4,16 +4,16 @@ import java.util.Random;
 class Cards {
     //declare an Array
 //    int[] deckOfCards;
-
     //define an arraylist
     ArrayList<Cards> packOfCards;
+    String suits[];
+    String cardNumber[];
     int rank;
     //Constructor for Arraylist
     public Cards(int capacity) {
         this.packOfCards = new ArrayList<>(capacity);
         this.rank =rank;
     }
-
     //Method to Generate Randomm Number
     void randomNumberGeneration(){
         double rank = Math.floor(Math.random()*10)%52;
@@ -28,25 +28,25 @@ class Cards {
             System.out.println("Card is Invalid");
         }
     }
-//    //Instance Variables
-//    int value_on_Card;
-//    String suits;
-//    //Constructor
-//    Cards() {
-//        // Used to store all 52 cards
-//        this.deckOfCards = new int[52];
-//    }
+    //Method to Show card
+    void showCard(){
+        for (int i=1;i<packOfCards.size(); i++){
+        System.out.println(i);
+        
+        }
+    }
 }
 public class DeckOfCard {
     public static void main(String[] args) {
         System.out.println("Welcome to play Deck of Cards");
-/       String [] suits={"Daimond","Hearts", "Spade", "Clubs"};
+         String [] suits={"Diamond","Hearts", "Spade", "Clubs"};
         String [] cardNumber ={"2","3","4","5","6","7","8","9","10","Ace","King", "Queen", "Jack"};
         //Create an Object
         Cards dCards = new Cards(52);
         //Calling an ArrayList
         dCards.randomNumberGeneration();
-        
+        //Calling Show Method
+        dCards.showCard();
     }
 }
 
