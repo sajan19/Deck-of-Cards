85c95bf84ef6c3df4df9e4941aa33300bf1f7a86
Sajan <sajanmhatre29@gmail.com>
2021-09-09 09:01:50 +0530

    Declare cards

--- src/DeckOfCard.java
+++ src/DeckOfCard.java
@@ -3,19 +3,21 @@ class Cards {
     int[] deckOfCards;
     //Instance Variables
     int value_on_Card;
-    String ImageName;
+    String suits;
     //Constructor
     Cards() {
         // Used to store all 52 cards
         this.deckOfCards = new int[52];
     }
 }
-
 public class DeckOfCard {
     public static void main(String[] args) {
         System.out.println("Welcome to play Deck of Cards");
 //        UC1 : Setup the initial game with deck of cards and make sure we have unique cards, The numbe
 //        The number of cards are 52. Should be able to make unique deck. Maintain the ranks to further compare
+
+        String [] suits={"Ace","King", "Queen", "Jack"};
+        int [] cardNumber ={2,3,4,5,6,7,8,9,10};
         //Create an Object
         Cards dCards = new Cards();
 
